44fc3ec4c1c11b17a669b9de2b4c59e4cc718644
Rocio <rocioburgos00@gmail.com>
2021-10-17 21:53:36 -0300

    sprint 1.1

--- src/app/componentes/registro/registro.component.ts
+++ src/app/componentes/registro/registro.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroComponent implements OnInit {
 
+  paciente:boolean= false;
   constructor() { }
 
   ngOnInit(): void {
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyC22xa6jkjuAokYLpk6-yxGZqDPLCbGz7Q",
+    authDomain: "tpclinica-282c4.firebaseapp.com",
+    projectId: "tpclinica-282c4",
+    storageBucket: "tpclinica-282c4.appspot.com",
+    messagingSenderId: "280488327089",
+    appId: "1:280488327089:web:5dd19cb67214056891af90"
+  }
 };
 
 /*
